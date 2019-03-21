02efe945e8640b3e339fc508d1702dfc7bbbce82
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-21 11:52:28 +0900

    zshの設定ファイル読み込みを消し、シンボリックリンクがあった場合は上書き

--- setup.sh
+++ setup.sh
@@ -15,11 +15,5 @@ DOT_FILES=(
 
 for file in "${DOT_FILES[@]}"
 do
-    ln -s "$HOME"/dotfiles/"$file" "$HOME"/"$file"
+    ln -fs "$HOME"/dotfiles/"$file" "$HOME"/"$file"
 done
-
-# shellcheck disable=SC1090
-{
-    source ~/.zshenv
-    source ~/.zshrc
-}
