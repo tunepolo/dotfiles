e553680af5e7b0a459a6f0c2ec1ba172a121347d
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2019-09-14 09:16:21 +0900

    動かなくてイライラするので一旦削除

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -151,31 +151,5 @@ function 256colortest() {
   done
 }
 
-# Enterキーでlsとgit statusを実行する
-# http://qiita.com/yuyuchu3333/items/e9af05670c95e2cc5b4d
-function do_enter() {
-  if [ -n "$BUFFER" ]; then
-    zle accept-line
-    return 0
-  fi
-  echo
-  ls -a
-  # ↓おすすめ
-  # ls_abbrev
-  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]; then
-    echo
-    echo -e "\e[0;33m--- git status ---\e[0m"
-    git status -sb
-    echo
-    echo
-  fi
-  zle reset-prompt
-  return 0
-}
-
-
-zle -N do_enter
-bindkey '^m' do_enter
-
 # Go modulesをmodule-aware modeで使用する
 export GO111MODULE=auto
