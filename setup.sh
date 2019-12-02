6299f27fc3a8f1ef681c8b56893f2fe221660434
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2019-12-02 11:43:36 +0900

    git diff-highlightの設定を追加

--- setup.sh
+++ setup.sh
@@ -15,11 +15,13 @@ DOT_FILES=(
     .zshrc
 )
 
-for file in "${DOT_FILES[@]}"
-do
+for file in "${DOT_FILES[@]}"; do
     ln -fs "$HOME"/dotfiles/"$file" "$HOME"/"$file"
 done
 
 ./npm_install.sh
 ./go_install.sh
 
+# HomebrewでinstallしたGitのdiff-highlightを有効にする
+# https://udomomo.hatenablog.com/entry/2019/12/01/181404
+sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
