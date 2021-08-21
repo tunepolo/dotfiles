2bf711c4749cf0921d40b78c7fd6d9cfe9bdf638
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2021-08-22 07:54:55 +0900

    Goのツールインストールを見直し

--- go_install.sh
+++ go_install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Official CLI tools
-go get golang.org/x/tools/cmd/...
+go get golang.org/x/tools/cmd/... # Official CLI tools
 
-# REPL
-go get -u github.com/motemen/gore/cmd/gore
-go get -u github.com/mdempsky/gocode   # for code completion
-go get -u github.com/k0kubun/pp        # or github.com/davecgh/go-spew/spew
+go install github.com/x-motemen/gore/cmd/gore@latest # REPL
