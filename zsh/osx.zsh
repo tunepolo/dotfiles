07bbc8dd6b8cd7f04c23ff478b3e8d2732b4212d
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2026-01-17 11:58:11 +0900

    refactor: 元の設定ファイルを削除しchezmoi管理に完全移行
    - 元のdotfile(.zshrc, .vimrc等)を削除
    - 元の設定ディレクトリ(git/, vim/, zsh/)を削除
    - .chezmoiignoreを簡素化（ツール・ドキュメントのみ記載）
    
    設定ファイルはdot_プレフィックス付きファイルとprivate_ディレクトリで管理

--- zsh/osx.zsh
+++
@@ -1,8 +0,0 @@
-# setting for OS X
-
-alias ls="ls -AF -G"
-
-# LESSのソースハイライト
-# source-highlightをインストールする必要あり
-export LESS='-gj10 --no-init --quit-if-one-screen --RAW-CONTROL-CHARS'
-export LESSOPEN='| /usr/local/bin/src-hilite-lesspipe.sh %s'
